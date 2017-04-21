cdc888ba77b61581ab61844422b833a9c515e2c9
pershing <pershing@livesport.eu>
2017-04-21 14:29:48 +0200

    syntax tuning

--- check-huawei-switch.sh
+++ check-huawei-switch.sh
@@ -14,7 +14,7 @@ STATE_CRITICAL=2
 STATE_UNKNOWN=3
 
 # Var's
-VERBOSE="Y"
+VERBOSE="N"
 HOST="$1"
 MSG=""
 SNMPGET="snmpget -v2c -Oqv -r2 -t1 -c public $HOST"
@@ -26,7 +26,7 @@ SNMPWALK="snmpwalk -v2c -Oqn -r2 -t1 -c public $HOST"
 debug () {
     if [ "$VERBOSE" == "Y" ]; then
         echo -en "\n<DEBUG>"
-        echo -en $1
+        echo -en "$1"
         echo -en "</DEBUG>"
     fi
 }
@@ -39,7 +39,7 @@ check_operation_byindex () {
     while read -r line; do
         INDEX="$(echo "$line" | cut -d" " -f1 | awk -F. '{print $NF}')"
         INDEXHUMAN="$(echo "$line" | cut -d" " -f1 --complement)"
-        STATUS="$($SNMPGET 1.3.6.1.4.1.2011.5.25.31.1.1.1.1.2.$INDEX)"
+        STATUS="$(${SNMPGET} 1.3.6.1.4.1.2011.5.25.31.1.1.1.1.2."$INDEX")"
         if [ "$STATUS" != "3" ]; then
             RTRN="${NAME}:${INDEXHUMAN} Error;"
         fi
@@ -50,7 +50,7 @@ check_operation_byindex () {
 check_fans () {
     # list fans and their status and check it
     # not using function due to special indexing
-    FANS="`$SNMPWALK 1.3.6.1.4.1.2011.5.25.31.1.1.10.1.7`"
+    FANS="$(${SNMPWALK} 1.3.6.1.4.1.2011.5.25.31.1.1.10.1.7)"
     while read -r line; do
         INDEX="$(echo "$line" | cut -d" " -f1 | awk -F. '{print $NF"-"$(NF-1)}')"
         STATUS="$(echo "$line" | cut -d" " -f1 --complement)"
@@ -63,7 +63,7 @@ check_fans () {
 
 check_psu () {
     # get power id
-    POWERSLOTS="$($SNMPWALK 1.3.6.1.2.1.47.1.1.1.1.7 | grep "POWER [0-9]")"
+    POWERSLOTS="$(${SNMPWALK} 1.3.6.1.2.1.47.1.1.1.1.7 | grep "POWER [0-9]")"
     MSG="${MSG}$(check_operation_byindex "${POWERSLOTS}" "Power")"    
 }
 
@@ -80,34 +80,34 @@ check_general () {
 
 check_CE12800 () {
     # check cards
-    MPUS="$($SNMPWALK 1.3.6.1.2.1.47.1.1.1.1.7 | grep "MPU")"
-    MSG="${MSG}$(check_operation_byindex ${MPUS} "MPU")"
+    MPUS="$(${SNMPWALK} 1.3.6.1.2.1.47.1.1.1.1.7 | grep "MPU")"
+    MSG="${MSG}$(check_operation_byindex "${MPUS}" "MPU")"
  
-    SFUS="$($SNMPWALK 1.3.6.1.2.1.47.1.1.1.1.7 | grep "SFU")"
-    MSG="${MSG}$(check_operation_byindex ${SFUS} "SFU")"
+    SFUS="$(${SNMPWALK} 1.3.6.1.2.1.47.1.1.1.1.7 | grep "SFU")"
+    MSG="${MSG}$(check_operation_byindex "${SFUS}" "SFU")"
  
-    CMUS="$($SNMPWALK 1.3.6.1.2.1.47.1.1.1.1.7 | grep "CMU")"
-    MSG="${MSG}$(check_operation_byindex $CMUS "CMU")"
+    CMUS="$(${SNMPWALK} 1.3.6.1.2.1.47.1.1.1.1.7 | grep "CMU")"
+    MSG="${MSG}$(check_operation_byindex "${CMUS}" "CMU")"
  
-    LINECARDS="$($SNMPWALK 1.3.6.1.2.1.47.1.1.1.1.7 | grep "CE-L24LQ-EC1")"
-    MSG="${MSG}$(check_operation_byindex $LINECARDS "Linecard")"
+    LINECARDS="$(${SNMPWALK} 1.3.6.1.2.1.47.1.1.1.1.7 | grep "CE-L24LQ-EC1")"
+    MSG="${MSG}$(check_operation_byindex "${LINECARDS}" "Linecard")"
 }
 
 check_CE6851 () {
 
-    MAIN="$($SNMPWALK 1.3.6.1.2.1.47.1.1.1.1.7 | grep "\"CE68[0-9][0-9]-")"
-    MSG="${MSG}$(check_operation_byindex $MAIN "Switch")"
+    MAIN="$(${SNMPWALK} 1.3.6.1.2.1.47.1.1.1.1.7 | grep "\"CE68[0-9][0-9]-")"
+    MSG="${MSG}$(check_operation_byindex "${MAIN}" "Switch")"
 }
 
 ## MAIN
 
 # Check host
 if [ "$HOST" == "" ]; then
-    echo "Unknown host - check parameters"
+    echo "Unknown host - check parameters; USAGE: ./check-huawei-swich.sh HOST"
     exit $STATE_UNKNOWN
 fi
 # Get model & check snmp
-if ! MODEL=$($SNMPGET .1.3.6.1.4.1.2011.6.3.11.4.0 | tr -d "\""); then
+if ! MODEL=$(${SNMPGET} .1.3.6.1.4.1.2011.6.3.11.4.0 | tr -d "\""); then
     echo "SNMP Timeout"
     exit $STATE_UNKNOWN
 fi
